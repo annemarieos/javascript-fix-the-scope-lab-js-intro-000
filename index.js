de3e5dc546beb2d24a77d83f47bdb882cd6850f4
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-03 17:20:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  const two = 2
 
   // Feel free to move things around!
-  const two = 2
+  return parseInt(n, 10) + two
 }
 
 var funkyFunction = function() {
